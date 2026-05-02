0e4a9ac09cab8cbc3ee06d38b934a27348d4c43b
Ebby74 <amrhomes4845@gmail.com>
2026-05-03 00:35:00 +0800

    fix: add eslint config for ESLint v9

---
+++ eslint.config.mjs
@@ -0,0 +1,7 @@
+ import tseslint from 'typescript-eslint';
+    export default [
+      {
+        ignores: ['.next/**', 'node_modules/**'],
+      },
+      ...tseslint.configs.recommended,
+    ];
